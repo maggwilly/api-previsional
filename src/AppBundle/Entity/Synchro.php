e8b846f056801c699e9a64e8baa8d08110d0400e
PENKA <maggwilly03@gmail.com>
2017-02-24 15:08:59 +0100

    correctif du 24 fev.

--- src/AppBundle/Entity/Synchro.php
+++ src/AppBundle/Entity/Synchro.php
@@ -47,7 +47,7 @@ class Synchro
     private $visites;
 
     /**
-   * @ORM\OneToMany(targetEntity="AppBundle\Entity\PointVente", mappedBy="synchro", cascade={"persist","remove"})
+   * @ORM\OneToMany(targetEntity="AppBundle\Entity\PointVente", mappedBy="synchro", cascade={"persist"})
    *@ORM\OrderBy({"date" = "DESC"})
    */
     private $pointVentes;
