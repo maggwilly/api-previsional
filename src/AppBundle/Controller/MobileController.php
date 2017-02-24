ce0b8314f7ff843c440736480d43db6015902e31
PENKA <maggwilly03@gmail.com>
2017-02-24 15:24:54 +0100

    correctif du 24 fev.

--- src/AppBundle/Controller/MobileController.php
+++ src/AppBundle/Controller/MobileController.php
@@ -178,13 +178,13 @@ private function getConnectedUser(){
     public function apkAction()
 {
   $request = $this->get('request');
-    $path = $this->get('kernel')->getRootDir(). "/../web/home/apk/allreport_v1.0.8.0.apk";
+    $path = $this->get('kernel')->getRootDir(). "/../web/home/apk/allreport_v1.0.8.1.apk";
     $content = file_get_contents($path);
     $response = new Response();
 
     //set headers
     $response->headers->set('Content-Type', 'mime/type');
-    $response->headers->set('Content-Disposition', 'attachment;filename="allreport_v1.0.8.0.apk"');
+    $response->headers->set('Content-Disposition', 'attachment;filename="allreport_v1.0.8.1.apk"');
 
     $response->setContent($content);
     return $response;
