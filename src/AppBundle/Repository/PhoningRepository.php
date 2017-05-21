961ae79cc3c3ad570ea74f6402212f1567bb79d7
PENKA <maggwilly03@gmail.com>
2017-05-21 12:30:29 +0100

    21/05/2017

--- src/AppBundle/Repository/PhoningRepository.php
+++ src/AppBundle/Repository/PhoningRepository.php
@@ -31,22 +31,18 @@ class PhoningRepository extends EntityRepository
   
 
      public function findGroupByIssu($region=null, $startDate=null, $endDate=null){
-         $qb = $this->createQueryBuilder('p')->join('p.client','c');
-        if($region!=null){
-           $qb->where('c.ville=:ville')
-          ->setParameter('ville', $region);
+    $em = $this->_em;
+    $RAW_QUERY =($region!=null) ?'select p.issu, count(*) as nombre from (select *, p.issu from (select p.client_id,p.user_id, max(p.date) as date, max(p.heure) as heure, count(p.id) as nombre from phoning p where p.date>=:startDate and p.date<=:endDate group by p.client_id,p.user_id)lp join phoning p on (lp.client_id=p.client_id and lp.date=p.date and lp.heure=p.heure)  join client c on (lp.client_id=c.id and c.ville=:region) join user_account u on (lp.user_id=u.id)) p group by p.issu ;':'select p.issu, count(*) as nombre from (select *,p.issu from (select p.client_id,p.user_id, max(p.date) as date, max(p.heure) as heure, count(p.id) as nombre from phoning p where p.date>=:startDate and p.date<=:endDate group by p.client_id,p.user_id)lp join phoning p on (lp.client_id=p.client_id and lp.date=p.date and lp.heure=p.heure)  join client c on (lp.client_id=c.id) join user_account u on (lp.user_id=u.id)) p group by p.issu ;';
+    $statement = $em->getConnection()->prepare($RAW_QUERY);
+    if($region!=null){
+    $statement->bindValue('region', $region);
           }
-      if($startDate!=null){
-           $qb->andWhere('p.date>=:startDate')
-          ->setParameter('startDate', new \DateTime($startDate));
-          }
-          if($endDate!=null){
-           $qb->andWhere('p.date<=:endDate')
-          ->setParameter('endDate',new \DateTime($endDate));
-          }   
-          $qb->select('p.issu')->addSelect('count(p.id) as nombre')->groupBy('p.issu');
-
-          return $qb->getQuery()->getArrayResult();
+    $startDate=new \DateTime($startDate);
+    $endDate=new \DateTime($endDate);
+    $statement->bindValue('startDate', $startDate->format('Y-m-d'));
+    $statement->bindValue('endDate',  $endDate->format('Y-m-d'));
+    $statement->execute();
+      return  $result = $statement->fetchAll();
   }   
   
 
