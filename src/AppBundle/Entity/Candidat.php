8d12794587e6f36088ca66b6b5b42e9184e113db
PENKA <maggwilly03@gmail.com>
2017-06-24 21:59:20 +0100

    correctifs

--- src/AppBundle/Entity/Candidat.php
+++ src/AppBundle/Entity/Candidat.php
@@ -16,7 +16,7 @@ class Candidat
       /**
      * @var string
      * @ORM\Id
-     * @ORM\Column(name="studentId", type="string", length=255)
+     * @ORM\Column(name="studentId", type="string", length=255,unique=true)
      */
     private $studentId;
     /**
