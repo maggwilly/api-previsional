48dafee605750cdeafdd5b2595b10a7ae5a82389
PENKA <maggwilly03@gmail.com>
2017-07-02 12:29:31 +0100

    uid in place of email

--- src/AppBundle/Entity/Info.php
+++ src/AppBundle/Entity/Info.php
@@ -13,12 +13,6 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Info
 {
-       /**
-     * @var string
-     * @ORM\Id
-     * @ORM\Column(name="email", type="string", length=255, nullable=true)
-     */
-    private $email;
 
       /**
      * @var string
@@ -26,6 +20,12 @@ class Info
      * @ORM\Column(name="uid", type="string", length=255)
      */
     private $uid;
+
+       /**
+     * @var string
+     * @ORM\Column(name="email", type="string", length=255, nullable=true)
+     */
+    private $email;
 
 
     /**
