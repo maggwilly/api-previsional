4905e99841a52453e7512a7afc49658756139c7a
PENKA <maggwilly03@gmail.com>
2017-09-22 10:03:12 +0100

    allow-origin *

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new FOS\RestBundle\FOSRestBundle(),
             new Misteio\CloudinaryBundle\MisteioCloudinaryBundle(),
+            new Nelmio\CorsBundle\NelmioCorsBundle(),            
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
