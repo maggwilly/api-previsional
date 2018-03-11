b7c84b6f93922a004c4ecf8995ffc6dbbdf70a09
PENKA <maggwilly03@gmail.com>
2018-03-11 13:08:51 +0100

    resultat

--- src/AppBundle/Controller/PartieController.php
+++ src/AppBundle/Controller/PartieController.php
@@ -40,11 +40,11 @@ class PartieController extends Controller
          $mat = $em->getRepository('AppBundle:Matiere')->findOneById($request->query->get('matiere'));
          $parties=$matiere->getParties();
        foreach ($parties as $key => $partie) {
-             $partie->setIsAvalable(!empty($em->getRepository('AppBundle:Partie')->findAvalability(5,83)));
+             $partie->setIsAvalable(!empty($em->getRepository('AppBundle:Partie')->findAvalability($partie->getId(),$session->getId())));
             // $partie->setIsAvalable(true);
              $partie->setAnalyse($em->getRepository('AdminBundle:Analyse')->findOneOrNull( $info,$session,$mat,$partie)); 
          };
-        return  $em->getRepository('AppBundle:Partie')->findAvalability(5,83);
+        return  $parties;
     }
     
     /**
--- src/AppBundle/Repository/PartieRepository.php
+++ src/AppBundle/Repository/PartieRepository.php
@@ -18,7 +18,7 @@ class PartieRepository extends EntityRepository
     function findAvalability($partie,$session){
        $qb =$this->createQueryBuilder('a')
        ->where('a.id=:partie')->setParameter('partie', $partie)->leftJoin('a.sessions', 's');
-        return   $qb->andWhere('s.id=:sesion')->setParameter('sesion', $sesion)->getQuery()->getResult();
+        return   $qb->andWhere('s.id=:sesion')->setParameter('sesion', $session)->getQuery()->getResult();
     }
 
 
