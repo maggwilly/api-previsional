0d8fe7a00f42068f815d54ad9769e58f8f20244b
PENKA <maggwilly03@gmail.com>
2018-03-12 18:39:12 +0100

    resultat

--- src/Pwm/AdminBundle/Repository/RessourceRepository.php
+++ src/Pwm/AdminBundle/Repository/RessourceRepository.php
@@ -15,7 +15,7 @@ class RessourceRepository extends \Doctrine\ORM\EntityRepository
        $now->modify('-15 day');
          $qb = $this->createQueryBuilder('r')
          ->where('r.session=:session or r.isPublic=:ispublic')->setParameter('session',$session)->setParameter('ispublic',true)
-          ->andWhere('a.date>:date')->setParameter('date',$now);
+          ->andWhere('r.date>:date')->setParameter('date',$now);
           return $qb->getQuery()->getResult();
   }
 
