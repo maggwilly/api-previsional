f04fc557a11cf9317e615d9dd93949887acf3742
PENKA <maggwilly03@gmail.com>
2018-04-30 10:43:20 +0100

    database upgrade

--- src/AppBundle/Event/CreateListener.php
+++ src/AppBundle/Event/CreateListener.php
@@ -69,14 +69,14 @@ public function onRegistration(RegistrationEvent $event)
 public function onUserCreated(InfoEvent $event)
 {  
       $info=$event->getInfo();
-      $registrations= $info->getRegistration();
+      $registrations= $info->getRegistrations();
       $notification = $this->_em->getRepository('MessagerBundle:Notification')->findOneId(580);
-        $notification ->setIncludeMail(true);
-        $this->_em->flush();
-          $registrationIds=$this->sendTo($registrations);
-           $data=array( 'page'=>'notification','id'=>$notification->getId());
-           $result=  $this->firebaseSend($registrationIds, $notification,$data); 
-          $this->controlFake($result,$registrations,$notification); 
+      $notification ->setIncludeMail(true);
+      $this->_em->flush();
+      $registrationIds=$this->sendTo($registrations);
+      $data=array( 'page'=>'notification','id'=>$notification->getId());
+      $result=  $this->firebaseSend($registrationIds, $notification,$data); 
+      $this->controlFake($result,$registrations,$notification); 
 }
 
 
