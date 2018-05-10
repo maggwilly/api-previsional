33ef45c71ece577c14ac401fa020288e44dd62ad
PENKA <maggwilly03@gmail.com>
2018-05-10 20:54:04 +0100

    un

--- src/Pwm/AdminBundle/Repository/InfoRepository.php
+++ src/Pwm/AdminBundle/Repository/InfoRepository.php
@@ -22,7 +22,7 @@ class InfoRepository extends \Doctrine\ORM\EntityRepository
    public function findNotProfilFilled(){
          $qb = $this->createQueryBuilder('a')
          ->where('a.phone is NULL or a.niveau is NULL or a.serie is NULL');
-          return $qb->getQuery()->getResult();
+          return $qb->getQuery()->setMaxResults(50)->getResult();
   }
 
    public function findSubscribersExpired(){
