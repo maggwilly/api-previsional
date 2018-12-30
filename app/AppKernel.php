aeb99c64a9bd23edf12291bd789b0b7f1a53c685
Williams Penka <williams.penka@gmail.com>
2018-12-30 16:10:07 +0100

    first deploy

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,13 +22,13 @@ class AppKernel extends Kernel
             new Nelmio\CorsBundle\NelmioCorsBundle()
         );
 
-        if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
+       /* if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
             $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
-        }
+        }*/
 
         return $bundles;
     }
